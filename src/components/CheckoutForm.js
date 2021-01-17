dbbb115f5e0060d1ef30a2f0c06f3fd0ecdc6eba
TaQuangVo <danxawe@gmail.com>
2021-01-17 17:13:12 +0100

    did chechout validation

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -11,8 +11,9 @@ import {CardElement, useStripe, useElements} from "@stripe/react-stripe-js";
 export default function CheckOutForm() {
 
   const [isProcessing, setIsProcessing] = useState(false);
-  const [chekoutError, setChekoutError] = useState();
-
+  const [isCardFormComplete, setIsCardformComplete] = useState(false);
+  const [isNameFormComplete, setIsNameformComplete] = useState(false);
+  const [checkoutError, setChekoutError] = useState("");
   const [name, setName] = useState("");
   const [email, setEmail] = useState(""); 
 
@@ -44,27 +45,40 @@ export default function CheckOutForm() {
           color: '#ffc7ee',
         },
       },
-    };
+  };
   const handleNameChange = (e) =>{
     setName(e.target.value);
+    if(e.target.value === ""){
+      setIsNameformComplete(false);
+    }else{
+      setIsNameformComplete(true);
+    }
   }
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
   }
   const onCardChange = (e) => {
-    if(e.complete ===true){
-      console.log("complete");
-    }
-    if(e.complete ===false){
-      console.log("incomplete");
-    }
+
+    setIsCardformComplete(e.complete);
+
     if(e.error !== undefined){
       console.log(e.error);
+      setChekoutError(e.error.message);
+    }else{
+      setChekoutError("");
     }
   }
+
   const handleFormSubmit = async (e) => {
-    setIsProcessing(true); 
     e.preventDefault();
+
+    if(isProcessing){
+      setChekoutError("Processing...");
+    }else if(!isCardFormComplete || !isNameFormComplete){
+      setChekoutError("Fyll i nödvändiga rutor!");
+    }
+    else{
+    setIsProcessing(true); 
 
     //preset payload for geting clientsecret,
     const payload = [];
@@ -86,34 +100,37 @@ export default function CheckOutForm() {
     const confirmOrder = await axios.post("https://us-central1-orientk-23b4b.cloudfunctions.net/api/payment_intents",payload);
 
     if(confirmOrder.data.client_secret == null){
-
-      setIsProcessing(false);
-      console.log("nått gick fel");
-
+      console.log("nått gick fel med att hämta client_secrett från stripe, kan va någon i meny inte finns längre i databasen ");
+      setChekoutError("Något gick fel!")
     }else{
-
       //get cardElement and billing ditails,
       const cardElement = elements.getElement(CardElement);
       const billingDetails = {
         name: name,
-        email: email,
+        email: (email=="")? null : email,
       }
-
       //requse payment medthod;
       const paymentMethodReq = await stripe.createPaymentMethod({
         type:"card",
         card: cardElement,
         billing_details:billingDetails,
       })
-      //confirm cardpayment
-      const confirmCardPayment = await stripe.confirmCardPayment(confirmOrder.data.client_secret,{
-        payment_method: paymentMethodReq.paymentMethod.id,
-      });
-
-      console.log(paymentMethodReq);
-      console.log(confirmCardPayment);
-      console.log(confirmOrder);
+      if(paymentMethodReq.paymentMethod == undefined){
+        setChekoutError(paymentMethodReq.error.message) ;
+        console.log("nått gick fel med att skappfa en betalningmetod, kan va att att fel kortnr");
+        
+      }else{
+        //confirm cardpayment
+        const confirmCardPayment = await stripe.confirmCardPayment(confirmOrder.data.client_secret,{
+          payment_method: paymentMethodReq.paymentMethod.id,
+        });
+        console.log(confirmOrder);
+        console.log(paymentMethodReq);
+        console.log(confirmCardPayment);
+      }
     }
+    setIsProcessing(false);
+  }
 
   }
 
@@ -126,7 +143,11 @@ export default function CheckOutForm() {
           <div  className="FormRow">
               <CardElement onChange={(e)=>{onCardChange(e)}} options={CARD_OPTIONS} />
           </div>
-          <button onClick={(e)=>{handleFormSubmit(e)}} type="submit" disabled={isProcessing}>{isProcessing ? "Loading..." : "Betala"}</button>
+          <button onClick={(e)=>{handleFormSubmit(e)}} type="submit" 
+          disabled={(isProcessing || !isNameFormComplete || !isCardFormComplete)}>
+            {isProcessing ? "Loading..." : "Betala"}
+          </button>
+          <p className="checkout__error">{checkoutError}</p>
       </form>
     )
 }
